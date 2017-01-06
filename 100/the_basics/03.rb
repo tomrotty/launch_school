7e3212ab74f8f8f4a65eb9bff132a281615f3b1f
Tom Rotty <tomrotty@gmail.com>
2017-01-06 14:40:22 +0100

    Exercise 1,2,3 finished

---
+++ 100/the_basics/03.rb
@@ -0,0 +1,13 @@
+movies = {
+  "Interstellar" => "2014",
+  "Arrival" => "2016",
+  "Prisoners" => "2013",
+  "Eyes Wide Shut" => "1999",
+  "The Game" => "1997"
+}
+
+puts movies["Interstellar"]
+puts movies["Arrival"]
+puts movies["Prisoners"]
+puts movies["Eyes Wide Shut"]
+puts movies["The Game"]
